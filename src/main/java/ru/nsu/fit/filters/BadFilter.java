0608e5705233d1c6deb7a3f38ab3f6b8f59da6f5
Shabalin Artyom <artem2010_01@mail.ru>
2023-04-03 16:13:10 +0700

    add fantasy pink filter

--- src/main/java/ru/nsu/fit/filters/BadFilter.java
+++ src/main/java/ru/nsu/fit/filters/BadFilter.java
@@ -7,19 +7,24 @@ import java.util.List;
 public class BadFilter implements Filter{
     @Override
     public BufferedImage execute(BufferedImage originalImage) {
-        BufferedImage newImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        BufferedImage result = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
         for (int x = 0; x < originalImage.getWidth(); x++) {
             for (int y = 0; y < originalImage.getHeight(); y++) {
                 int rgb = originalImage.getRGB(x, y);
-                float r = (float)((rgb & 0x00FF0000) >> 16); // красный
-                float g = (float)((rgb & 0x0000FF00) >> 8); // зеленый
-                float b = (float)(rgb & 0x000000FF); // синий
-                r = g = b = (int) (0.900 * r + 0.50 * g + 0.50 * b);
-                int newPixel = 0xFF000000 | ((int)r << 16) | ((int)g << 8) | ((int)b);
-                newImage.setRGB(x, y, newPixel);
+                float R = (float)((rgb & 0x00FF0000) >> 16); // красный
+                float G = (float)((rgb & 0x0000FF00) >> 8); // зеленый
+                float B = (float)(rgb & 0x000000FF); // синий
+                /*var newR = (int) (0.393 * R + 0.769 * G + 0.189 * B);
+                newR = (newR>255)? 255:Math.max(newR, 0);
+                var newG = (int) (0.349 * R + 0.686 * G + 0.168 * B);
+                newG = (newG>255)? 255:Math.max(newG, 0);
+                var newB = (int) (0.272 * R + 0.534 * G + 0.131 * B);
+                newB = (newB>255)? 255:Math.max(newB, 0);*/
+                int newPixel = 0xFF000000 | ((int)(R*0.937) << 16) | ((int)(G*0.784) << 8) | ((int)(B*0.772));
+                result.setRGB(x, y,newPixel);
             }
         }
-        return newImage;
+        return result;
     }
 
     @Override
